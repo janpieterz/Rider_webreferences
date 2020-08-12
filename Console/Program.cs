4cd5b27c7fd9158a82483a03b3edd4f0220f2d63
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2020-08-12 08:35:52 +0200

    Sale and Site service added

---
+++ Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WebServiceConsole.MindBody.SaleService;
+using WebServiceConsole.MindBody.SiteService;
+
+namespace WebServiceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SaleServiceSoapClient saleClient = new SaleServiceSoapClient();
+            var result = saleClient.GetContracts(new GetContractsRequest());
+            SiteServiceSoapClient siteClient = new SiteServiceSoapClient();
+            //Should not be void
+            siteClient.GetLocations();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
